f358d58b55d1b87f9b61ce649f70b496956c33ec
Bulatik <kmpchk@gmail.com>
2021-06-14 23:32:33 +0300

    Delete files

--- src/Network.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <chrono>
-
-int main() {
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(
-            std::chrono::system_clock::now().time_since_epoch()).count() << std::endl;
-    return 0;
-}
