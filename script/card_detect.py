4d04e2709c567953149d1ca9e45af255c55e277c
kmpchk <kmpchk@gmail.com>
2021-10-21 17:32:29 +0300

    Update card_detect.py

--- script/card_detect.py
+++ script/card_detect.py
@@ -1,44 +1,75 @@
 #!/usr/bin/env python3
 
-from std_msgs.msg import Float32
+from std_msgs.msg import Int8
 import cv2
 import rospy
 import numpy as np
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+cap = cv2.VideoCapture(4)  # /dev/video4
+
+#cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 #cap.set(cv2.CAP_PROP_BRIGHTHNESS, 0)
-cap.set(cv2.CAP_PROP_CONTRAST, 0)
+#cap.set(cv2.CAP_PROP_CONTRAST, 0)
 
 if __name__ == "__main__":
 
     rospy.init_node('card_detect')
-    pub = rospy.Publisher('/card_detect', Float32, queue_size=10)
+    pub = rospy.Publisher('/card_detect', Int8, queue_size=10)
     rate = rospy.Rate(10)
 
-    while(cap.isOpened() and not rospy.is_shutdown()):
+    while (cap.isOpened() and not rospy.is_shutdown()):
         ret, frame = cap.read()
-        if ret == True:
-            hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-            lower_red = np.array([110, 0 , 0])
-            upper_red = np.array([130, 255, 255])
-            mask = cv2.inRange(hsv, lower_red, upper_red)
-            #print(hsv[240][320])
-            
-            lower_red = np.array([45, 0, 0])
-            upper_red = np.array([70, 255, 255])
-            mask2 = cv2.inRange(hsv, lower_red, upper_red)
-            #cv2.imshow('Frame', mask2)
-            #cv2.waitKey(1) 
-            
-            red = np.sum(mask.astype('bool')) / np.shape(mask)[0] / np.shape(mask)[1]
-            green = np.sum(mask2.astype('bool')) / np.shape(mask2)[0] / np.shape(mask2)[1]
-            #print('Red: ', red)
-            #print('Green: ', green)
-            pub.publish(red - green)
-        else: 
+
+        if not ret:
             break
-            
+
+
+        ret, frame = cap.read()
+
+        cv2.imshow('frame', frame)
+
+        # RED
+        hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+        lower_red = np.array([118, 0, 0])
+        upper_red = np.array([128, 255, 255])
+        red_mask = cv2.inRange(hsv, lower_red, upper_red)
+        cv2.imshow('RED mask', red_mask)
+        #print(np.shape(hsv))
+        square = np.shape(hsv)[0] * np.shape(hsv)[1]
+        print(hsv[240][320])
+
+        # GREEN
+        lower_green = np.array([42, 100, 20])
+        upper_green = np.array([60, 255, 255])
+        green_mask = cv2.inRange(hsv, lower_green, upper_green)
+        cv2.imshow('GREEN mask', green_mask)
+
+        # Calculate count
+        red_sum = np.sum(red_mask.astype("bool")) / square
+        green_sum = np.sum(green_mask.astype("bool")) / square
+        red = red_sum > 0.1
+        green = green_sum > 0.1
+
+        # Publish to ROS topic
+        print('Red detected: ', red)
+        print('Green detected: ', green)
+
+        if red and green:
+            if red_sum > green_sum:
+                pub.publish(-1)
+            else:
+                pub.publish(1)
+        elif red:
+            pub.publish(-1)
+        elif green:
+            pub.publish(1)
+        else:
+            pub.publish(0)
+
+        # Display image
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
         rate.sleep()
 
     cap.release()
