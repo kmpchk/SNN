d8ac83bf862eb712b8b5c290332f9f5959105e94
Bulatik <kmpchk@gmail.com>
2021-10-29 01:47:57 +0300

    Add Utils.h

--- src/Utils.h
+++ src/Utils.h
@@ -1,14 +1,23 @@
-//
-// Created by optio32 on 10/28/21.
-//
-
 #ifndef SNN_UTILS_H
 #define SNN_UTILS_H
 
+// Spdlog
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 
-class Utils {
+inline constexpr auto MAX_LOG_FILE_SIZE_BYTES = 50 * 1024 * 1024;
 
-};
+// Timer
+#include <chrono>
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+
+void init_logger(spdlog::level::level_enum terminal_log_level,
+                 spdlog::level::level_enum file_log_level);
 
 
 #endif //SNN_UTILS_H
